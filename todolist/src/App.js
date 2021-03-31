90d33bafb7e1408869a2f17a4c0a7a2e986bd0f9
chenerychen <zhaocsnh@gmail.com>
2021-03-30 23:27:26 -0400

    First Commit of my todoList from Scrimba, this is a guided project but I rewrote it from scratch

---
+++ todolist/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import TodoItems from './TodoItems'
+import todosData from './todosData' 
+
+class App extends React.Component {
+  constructor() {
+    super() 
+    this.state={
+      todos: todosData 
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedStates = prevState.todos.map(todo => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed
+          }
+          return todo 
+      })
+      return {todos: updatedStates}  
+    })
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => <TodoItems key={item.id} item={item} handleChange={this.handleChange} />)
+    
+
+    return (
+        <div> 
+          {todoItems}
+        </div>
+    )
+  }
+
+}
+
+export default App
+
+
---
+++ todolist/src/TodoItems.js
@@ -0,0 +1,18 @@
+import React from 'react' 
+
+
+function TodoItems(props) {
+    return (
+        <div>
+        <input 
+               type='checkbox'
+               checked={props.item.completed}
+               onChange={ () => 
+                props.handleChange(props.item.id)} 
+                /> 
+            <p>{props.item.text}</p>
+        </div> 
+    )
+}
+
+export default TodoItems 
