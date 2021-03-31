871749fa75bf04a707f7e5d925424f3cae04ff39
chenerychen <zhaocsnh@gmail.com>
2021-03-31 15:23:59 -0400

    commited Conditioanl Rendering use state and setState and componentDidMount and Settimeout to mimic loading and transition from loading to contents

---
+++ conditional-render/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react' 
+import Conditional from "./Conditional"
+
+
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state={
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoading: false 
+      })
+    }, 1000)
+  }
+
+  // render() {
+  //   return (
+  //     <div> 
+  //       <Conditional isLoading={ this.state.isLoading}  />
+  //     </div>
+  //   )
+  // }
+
+    render() {
+    return (
+      <div> 
+        {this.state.isLoading ? 
+          <h1>Loading...</h1> : 
+        <Conditional  /> 
+    } 
+
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+export default App
---
+++ conditional-render/src/Conditional.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+
+// function Conditional(props) {
+//     return ( 
+//         <div> 
+//             {props.isLoading ? <h1> Loading...</h1> : <h1> Page is ready!</h1>} 
+//         </div>
+//     ) 
+// }
+
+function Conditional(props) {
+    return ( 
+        <div> 
+            <h1> Page is ready!</h1>
+        </div>
+    ) 
+}
+
+export default Conditional 
+
