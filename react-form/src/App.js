345e4abcb6e0615567f855105346efd65dacc36a
chenerychen <zhaocsnh@gmail.com>
2021-04-01 16:06:04 -0400

    commit react form with checkbox, radio, select and textarea functionalities

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,51 +1,104 @@
-import React from 'react' 
+import React from 'react'
+
 
 
 class App extends React.Component {
 
-  constructor() {
+  constructor () {
     super() 
     this.state={
       firstName: "", 
-      lastName: "" 
+      lastName:"", 
+      legalAge: false, 
+      favColor: '' 
     }
-  this.handleChange = this.handleChange.bind(this)
-  } 
+    this.handleChange = this.handleChange.bind(this)
+  }
 
   handleChange(event) {
-    const {name, value} = event.target 
-    this.setState({
-      [name] : value
-    })
+      const {name, value, type, checked} = event.target
+      type === 'checkbox' ? this.setState({[name]: checked }) : this.setState({[name] : value })
   }
 
-  
-  
   render () {
     return (
-      <form> 
+      <form>
           <input 
-          type='text' 
-          name='firstName' 
-          value={this.state.firstName} 
-          placeholder="First Name"
-          onChange={this.handleChange}
+            type='text'
+            name='firstName'
+            value={this.state.firstName}
+            onChange={this.handleChange}
+            placeholder="First Name"
           /> 
-       <br/> 
-        <input 
-          type='text' 
-          name='lastName' 
-          value={this.state.lastName} 
-          placeholder="Last Name"
-          onChange={this.handleChange}
+
+          <br/> 
+
+          <input 
+            type='text'
+            name='lastName'
+            value={this.state.lastName}
+            onChange={this.handleChange}
+            placeholder="Last Name"
           /> 
-        <h1>
-          {this.state.firstName} {this.state.lastName}
-        </h1>
+
+          <br/> 
+
+          <textarea 
+            value="Leave your feedback here."
+            onChange={this.handleChange}
+          />
+
+
+          <br/> 
+
+         <label>
+          <input 
+            type='checkbox'
+            name="legalAge"
+            checked={this.state.legalAge}
+            onChange={this.handleChange}
+            />Is legal to drink?
+          </label>
+
+      <label>
+        <input
+            type="radio"
+            name='gender'
+            value='male' 
+            onChange={this.handleChange}
+            checked={this.state.gender ==='male'}
+        />   Male
+      </label> 
+
+      <label>
+        <input
+            type="radio"
+            name='gender'
+            value='female' 
+            onChange={this.handleChange} 
+            checked={this.state.gender === 'female'}
+        />   Female
+      </label> 
+
+      <select
+        value={this.state.favColor} 
+        onChange={this.handleChange}
+        name="favColor"
+      > 
+      <option value="red"> Red</option>
+      <option value='black'> Black </option> 
+
+      </select>
+
+
+       <h1> {this.state.firstName} {this.state.lastName} </h1> 
+
 
       </form>
+
     )
   }
+
 }
 
 
